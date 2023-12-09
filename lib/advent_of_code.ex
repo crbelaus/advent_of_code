7b36adf7b20fb4217f47165026fb7029deb2c7f5
crbelaus <cristian@crbelaus.com>
2023-12-09 13:32:07 +0100

    Initial commit

---
+++ lib/advent_of_code.ex
@@ -0,0 +1,8 @@
+defmodule AdventOfCode do
+  def input(year, day) do
+    ["lib", "#{year}", "day-#{day}", "day-#{day}-input.txt"]
+    |> Path.join()
+    |> File.read!()
+    |> String.split("\n")
+  end
+end
